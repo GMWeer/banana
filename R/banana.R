099734ba8b648315e5d85eb11df1d9fb56a5ce7f
Gayan Weerasooriya <93592647+GMWeer@users.noreply.github.com>
2022-03-24 01:55:21 +0530

    Pizza plot
    Null values have been created a  pizza plot

--- R/banana.R
+++ R/banana.R
@@ -2,16 +2,27 @@
 #' Calculating Data Frane Empty space
 #' @export
 #' @param data name of data frame
-banana = function(data){
+banana = function(data,list1,list2){
+  library(ggplot2)
+  list1 <- c()
+  list2 <- c()
   for ( o in colnames(data)) {
-
+    
     l=sum(is.na(data[o]))
-
+    #c=length(data[o])
+    list1=c(list1, o)
+    list2=c(list2, l)
+  
+    #m = l/c
     print(o)
     print('missing values')
     print(l)
     print('=================')
-
+    #print(c)
   }
-
+  z <- data.frame( a=list1,  b=list2)
+  cxc <- ggplot(z, aes(x=a, y=b, fill=factor(b))) + 
+    geom_bar(width = 1,stat="identity",colour = "black")
+  cxc + coord_polar() + 
+    theme_linedraw() +theme(axis.ticks =element_blank(), axis.text.y =element_blank(), axis.title=element_blank(), axis.text.x=element_text(size = 12,angle = 45)) 
 }
